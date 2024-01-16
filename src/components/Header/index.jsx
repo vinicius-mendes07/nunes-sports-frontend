c848005388bc5bf87061f63032f2b136fc494981
vinicius <vinicius.henrique.92@hotmail.com>
2024-01-16 11:45:52 -0300

    feat: created the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,15 @@
+import { Container } from "./styles";
+import nunesSports from '../../assets/images/nunesSports.png'
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <h1>header #0092CA</h1>
+  return (
+    <Container>
+      <img src={nunesSports} alt="nunes sports logo" />
+      <div>
+        <Link to="/">Inicio</Link>
+        <Link to="/new">Criar novo produto</Link>
+      </div>
+    </Container>
+  )
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  padding: 24px 64px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+
+  div {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    font-weight: bold;
+
+    a {
+      text-decoration: none;
+      color: ${({ theme }) => theme.colors.gray[900]};
+      color: ${({ theme }) => theme.colors.primary.main};
+      transition: 0.2s ease-in;
+
+      &:hover {
+        color: ${({ theme }) => theme.colors.primary.dark};
+      }
+    }
+  }
+
+  img  {
+    border-radius: 4px;
+    width: 150px;
+  }
+`
