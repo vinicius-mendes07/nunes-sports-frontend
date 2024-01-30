600796ba1662defdf6c93017b6c87814321dd923
vinicius <vinicius.henrique.92@hotmail.com>
2024-01-30 19:33:31 -0300

    refactor: removed unnecessary comments

--- src/Router.jsx
+++ src/Router.jsx
@@ -25,37 +25,4 @@ const Router = createBrowserRouter([
   },
 ])
 
-//   {
-//     path: "/",
-//     element: <RootLayout />,
-//     children: [
-//         {
-//             index: true,
-//             element: <Home />
-//         },
-//     ]
-// },
-// {
-//     path: "/items",
-//     element: <ItemsLayout />,
-//     children: [
-//         {
-//             index: true,
-//             element: <StockItems />
-//         },
-//         {
-//             path: "item/:itemId",
-//             element: <Item />
-//         },
-//         {
-//             path: "item/new",
-//             element: <CreateItem />
-//         },
-//         {
-//             path: "item/update/:itemId",
-//             element: <UpdateItem />
-//         }
-//     ]
-// },
-
 export default Router;
